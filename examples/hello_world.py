77c74b084a9064ac356d395f3ce90f69a947f50b
Kevin McAleer <kevinmcaleer@gmail.com>
2026-06-19 23:44:47 +0100

    fix: improve formatting in hello_world.py and add test.yml example

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -1,10 +1,21 @@
 # Example Hello World Program
+# Created for kevsrobots.com
+
 message = "Hello World"
 
-def hello(msg:str="Hi"):
+def hello(msg: str = "Hi"):
+    """
+    Prints the provided message to the console.
+
+    Args:
+        msg (str): The message to print. Defaults to "Hi".
+    """
     print(msg)
 
 hello(message)
 
 def test():
+    """
+    Placeholder function for future implementation.
+    """
     pass
